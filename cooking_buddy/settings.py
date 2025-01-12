33f3df10b992f0551d3648cea5dae20407e7bac7
Arsalan Naziri <arsalannaziri0786@gmail.com>
2025-01-12 12:34:46 +0530

    new commit

--- cooking_buddy/settings.py
+++ cooking_buddy/settings.py
@@ -17,6 +17,14 @@ import sys
 import json
 import base64
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('This is a debug message') 
+logging.info('This is an info message') 
+logging.warning('This is a warning message') 
+logging.error('This is an error message') 
+logging.critical('This is a critical message')
 
 env = Env(
     DEBUG=(bool, False),
